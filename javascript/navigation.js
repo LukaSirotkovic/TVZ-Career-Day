7f44776291621cbfb65ffc2a5a0f430ee12b3c9f
LukaSirotkovic <lukasirotkovic5@gmail.com>
2025-04-25 16:08:19 +0200

    Created 1440px desktop version. Responsivnes needed.

---
+++ javascript/navigation.js
@@ -0,0 +1,7 @@
+
+const hamburger = document.querySelector('.hamburger');
+const navLinks = document.querySelector('.navigation ul');
+
+hamburger.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
+});
