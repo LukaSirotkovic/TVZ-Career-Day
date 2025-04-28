d11784a7b748221cc3074d73f0ac591cdef12d24
LukaSirotkovic <lukasirotkovic5@gmail.com>
2025-04-28 22:50:07 +0200

    Created media query for navigation, header, countdown section and footer. Added button and text underneath the hero image. Added hamburger icon with animation.

--- javascript/navigation.js
+++ javascript/navigation.js
@@ -1,7 +1,28 @@
 
 const hamburger = document.querySelector('.hamburger');
-const navLinks = document.querySelector('.navigation ul');
+const mobileMenu = document.querySelector('.mobile-menu');
 
 hamburger.addEventListener('click', () => {
-    navLinks.classList.toggle('active');
+    hamburger.classList.toggle('is-active');
+    mobileMenu.classList.toggle('active');
+    navigation.classList.remove('scrolled');
 });
+
+const navigation = document.querySelector('.navigation');
+// Ako je mobile meni aktivan, nemoj raditi scroll efekat
+
+function handleScroll() {
+    // Ako je mobile meni aktivan, nemoj raditi scroll efekat
+    if (mobileMenu.classList.contains('active')) {
+        return;
+    }
+
+    if (window.scrollY > 115) {
+        navigation.classList.add('scrolled');
+    } else {
+        navigation.classList.remove('scrolled');
+    }
+}
+
+
+window.addEventListener('scroll', handleScroll);
