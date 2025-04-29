0a3ebfb20005e56e51c5d10c9467ae8a0e565088
LukaSirotkovic <lukasirotkovic5@gmail.com>
2025-04-29 11:04:42 +0200

    Removed navigation in header and footer on mobile devices, added liks to social pages and changed icons.

--- javascript/navigation.js
+++ javascript/navigation.js
@@ -1,21 +1,7 @@
 
-const hamburger = document.querySelector('.hamburger');
-const mobileMenu = document.querySelector('.mobile-menu');
-
-hamburger.addEventListener('click', () => {
-    hamburger.classList.toggle('is-active');
-    mobileMenu.classList.toggle('active');
-    navigation.classList.remove('scrolled');
-});
-
 const navigation = document.querySelector('.navigation');
-// Ako je mobile meni aktivan, nemoj raditi scroll efekat
 
 function handleScroll() {
-    // Ako je mobile meni aktivan, nemoj raditi scroll efekat
-    if (mobileMenu.classList.contains('active')) {
-        return;
-    }
 
     if (window.scrollY > 115) {
         navigation.classList.add('scrolled');
